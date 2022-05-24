3c0a66eb3fc0bfaca273b741e1bf5f162ba153db
MohamedEZ-ZAROUALY <ezzaroualymuhammad@gmail.com>
2022-05-24 16:03:03 +0100

    fixed some css to sidebar and search respectively

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,8 +1,7 @@
-
-
 import { Button } from '@mui/material';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/image.png';
 import './sidebar.css'
 
 
@@ -19,6 +18,9 @@ function SideBar() {
     return (
     <div className="sidebar">
         <Link to='/'>
+        <div className='sidebar-logo'>
+         <img src={logo} alt="logo"/>
+        </div>
         <Button onClick={() => {setChosen(1)}} className={chosenClass(1)}>Dashboard</Button>
         <Button onClick={() => {setChosen(2)}} className={chosenClass(2)}>Explore</Button>
         <Button onClick={() => {setChosen(3)}} className={chosenClass(3)}>My Quizzes</Button>
