4ae9b969e12e8f39eb9dcf3e134eb07f2eaf13da
The7thHeart <elherzahmedonline@gmail.com>
2022-05-25 16:21:08 +0100

    facade for the websocket module

---
+++ src/socketsModule/facade/ParticipantFacade.ts
@@ -0,0 +1,41 @@
+import { Stomp } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import IdGenerator from "../generators/IdGenerator";
+import ParticipationController, { ParticipationObserver } from "../participationControllers/ParticipationController";
+import ParticipationAcceptenceReceiver from "../receivers/ParticipationAcceptenceReceiver";
+import ParticipationSender from "../senders/ParticipationSender";
+import WebSocketPublisher from "../websocket/WebSocketPublisher";
+import WebSocketSender from "../websocket/WebSocketSender";
+
+
+
+
+const participate = (pin:string, playerName:string, participationObserver:ParticipationObserver, setId:(id:string)=>void) => {
+
+
+
+    const idGenerator = new IdGenerator();
+    const stompClient = Stomp.over(new SockJS("http://localhost:8080/ws"));
+    const webSocketSender = new WebSocketSender(stompClient);
+    const participationController = new ParticipationController(new ParticipationSender(webSocketSender),idGenerator, participationObserver);
+
+    const webSocketPublisher = new WebSocketPublisher(stompClient);
+    const participationAcceptenceReceiver = new ParticipationAcceptenceReceiver(participationController);
+
+
+
+
+
+    const onConnected = () => {
+        
+        participationController.generateId()
+            .then(newId => { 
+                webSocketPublisher.subscribe(participationAcceptenceReceiver,"Players/"+newId+"/acceptence");
+                participationController.attendCompetition(pin,playerName)
+                setId(newId);
+            });
+    };
+    const onError = (err) => {console.log(err)};
+    stompClient.connect({}, onConnected, onError);
+
+}
