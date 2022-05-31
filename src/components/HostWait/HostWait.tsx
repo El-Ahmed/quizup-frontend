254e96ee359ac895415ca78399738ddf3930da98
The7thHeart <elherzahmedonline@gmail.com>
2022-05-31 10:04:26 +0100

    host now have a pin

---
+++ src/components/HostWait/HostWait.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './HostWait.css' ;
+import { createCompetition, nextQuestion, startCompetition } from "../../socketsModule/facade/HostFacade";
+import QuestionsController from "../../socketsModule/quizControllers/QuestionsController";
+import Question from "../../socketsModule/quizEntities/Question";
+import Quiz from "../../socketsModule/quizEntities/Quiz";
+
+
+export default function Details() {
+
+	const [pin, setPin] = useState('')
+    const [qc, setQC] = useState<QuestionsController>()
+    const [players, setPlayers] = useState<string[]>([])
+
+    const playerObserver = (players:string[]) => { setPlayers(players); };
+ 	const quiz = new Quiz('name','desc',[new Question("test1",[]),new Question("test2",[])]);
+    const host = ()=> {
+        createCompetition(quiz,playerObserver)
+        .then(qc => {
+            setQC(qc);
+            setPin(qc.getPin())
+            
+        } );
+    }
+    const start = () => {
+        if (qc)
+        startCompetition(qc);
+    }
+    const next = () => {
+        if (qc)
+        nextQuestion(qc);
+    }
+
+	useEffect(() => {
+	  host();
+	
+	}, [])
+	
+
+	
+	return (
+		<div className='app'>
+					<div className='theme'>
+							Movies / Chernobyl			
+					</div>
+					<div className='quizz-card'>
+						<div className='image-container'>
+							<img className='image' src="https://static.hbo.com/content/dam/hbodata/series/chernobyl/key-art/chernobyl-ka-1920.jpg" alt="paris"/>
+						</div>
+						<div className='quizz-informations'>
+							<h2>Code: {pin}</h2>
+						</div>
+					</div>				
+					<div className='details'>
+						<p className='titre'>Participants:</p>
+						<p>
+							{players.map((player)=> <>{player}</>)}
+						</p>
+					</div>
+					<div className='bouttons'>
+						<button  >Cancel</button>
+						<button onClick={start} >Start the Quiz</button>
+					</div>
+
+		</div>
+	);
+}
